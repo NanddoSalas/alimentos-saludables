887d553fddeea630be166b5d0059db121ee8373c
Fernando Salas <nanddosalas@gmail.com>
2021-11-01 14:02:20 -0700

    add HomePage

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Categories from '../components/Categories';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+
+const HomePage = () => (
+  <>
+    <Header />
+    <Hero />
+    <Categories />
+    <Footer />
+  </>
+);
+
+export default HomePage;
