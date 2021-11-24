46b9327ca9b92f3aa27e224023fb631015fa2ead
Fernando Salas <nanddosalas@gmail.com>
2021-11-24 16:13:36 -0700

    update stie metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,5 @@
 export const siteMetadata = {
-  title: 'Alimentos Saludables',
+  title: 'AlimentoSaludables',
   titleTemplate: '%s · Alimentos Saludables',
   description: 'Mas de 150 Monografias de Alimentos Saludables!',
   siteUrl: 'https://losalimentosaludables.com',
